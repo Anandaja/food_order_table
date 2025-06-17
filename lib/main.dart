87e613386eb3b344f2faa227ab595fc038f9e49f
Anandaja <anandajayan2003@gmail.com>
2025-06-17 20:50:17 +0530

    code for food order table

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:order_now_android/firebase_options.dart';
-import 'package:order_now_android/view/homepage.dart';
-import 'package:order_now_android/view/landingpage.dart';
-import 'package:order_now_android/view_model/cartpage_view_medel.dart';
-import 'package:order_now_android/view_model/homepage_view_model.dart';
-import 'package:order_now_android/view_model/homepageuser_view_model.dart';
-import 'package:order_now_android/view_model/landing_page_view_model.dart';
-import 'package:order_now_android/view_model/order_details_view_model.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'package:order_now_android/firebase_options.dart';
+// import 'package:order_now_android/view/homepage.dart';
+// import 'package:order_now_android/view/landingpage.dart';
+// import 'package:order_now_android/view_model/cartpage_view_medel.dart';
+// import 'package:order_now_android/view_model/homepage_view_model.dart';
+// import 'package:order_now_android/view_model/homepageuser_view_model.dart';
+// import 'package:order_now_android/view_model/landing_page_view_model.dart';
+// import 'package:order_now_android/view_model/order_details_view_model.dart';
 import 'package:provider/provider.dart';
 
 
